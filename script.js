35a6a3e6b674a821463e34edb0ce457313e04b70
TomKeny <tomkenyon2003@gmail.com>
2023-11-08 14:38:55 +0000

    Added multiple species options and improved playability

--- script.js
+++ script.js
@@ -94,8 +94,8 @@ class tamagotchi {
     GetStats() {
         return console.table({
             name: this.name,
-            health: this.health,
-            hunger: this.hunger
+            health: this.health + "%",
+            hunger: this.hunger + "%"
         })
     }
     Level() {
@@ -114,12 +114,12 @@ class tamagotchi {
         this.health += 10
         this.hunger -= 30
         this.xp += 2
-        console.log(`${this.name} enjoyed their food!`)
+        console.log(`${this.name} enjoyed their food! \n +2xp`)
     }
     Play() {
         this.hunger += 10
         this.xp += 4
-        console.log(`${this.name} had fun playing with you!`)
+        console.log(`${this.name} had fun playing with you! \n +4xp`)
     }
 }
 
@@ -127,62 +127,124 @@ class bird extends tamagotchi {
     constructor (name, flightSpeed) {
         super (name)
         this.flightSpeed = flightSpeed
+        this.flightTime = 30
+        this.species = "bird"
     }
     GetStats() {
         return console.table({
             name: this.name,
-            health: this.health,
-            hunger: this.hunger,
-            flightSpeed: this.flightSpeed
+            health: this.health + "%",
+            hunger: this.hunger + "%",
+            flightSpeed: this.flightSpeed + "m/s",
+            flightTime: this.flightTime + "s"
         })
     }
     Fly () {
         this.health += 10
         this.hunger += 20
         this.xp += 6
-        console.log(`${this.name} flew ${this.flightSpeed * 30}m in 30 seconds, and they had a blast doing it!`)
-        console.log(`${this.name} got a bit better at flying! FlightSpeed: ${this.flightSpeed} + 1`)
+        console.log(`${this.name} flew ${this.flightSpeed * this.flightTime}m in ${this.flightTime} seconds, and they had a blast doing it!`)
+        console.log(`${this.name} got a bit better at flying! \n FlightSpeed: ${this.flightSpeed}m/s + 1m/s \n FlightTime: ${this.flightTime}s + 3s \n +6xp`)
         this.flightSpeed += 1
+        this.flightTime += 3
+    }
+}
+
+class fish extends tamagotchi {
+    constructor (name, swimSpeed) {
+        super (name)
+        this.swimSpeed = swimSpeed
+        this.species = "fish"
+    }
+    GetStats() {
+        return console.table({
+            name: this.name,
+            health: this.health + "%",
+            hunger: this.hunger + "%", 
+            swimSpeed: this.swimSpeed + "m/s"
+        })
+    }
+    Swim () {
+        this.health += 10
+        this.hunger += 20
+        this.xp += 6
+        console.log(`${this.name} swam ${this.swimSpeed * 30}m, and they had a great time doing it!`)
+        console.log(`${this.name} improved at swimming! \n swimSpeed: ${this.swimSpeed} + 1 \n +6xp`)
+        this.swimSpeed += 1
     }
 }
 
 const prompt = require('prompt-sync')();
 
-let input
+let input,species,name,speed,obj
 let exit = false
 
-let name = prompt("Name Your Tamogotchi ")
-let speed = prompt("How fast can your tamagotchi fly (m/s)! ")
-
-const obj = new bird(name,Number(speed))
+name = prompt("Name Your Tamogotchi ")
+while (species != "fish" && species != "bird") {
+    species = prompt(`Is ${name} a bird or a fish `)
+    if (species == "bird") {
+        speed = prompt(`How fast can ${name} fly (m/s)! `)
+        obj = new bird(name,Number(speed))
+    }
+    else if (species == "fish") {
+        speed = prompt(`How fast can ${name} swim (m/s)! `)
+        obj = new fish(name,Number(speed))
+    }
+    else {
+        console.log("please select one of the options (case sensitive)")
+    }
+}
 
 while (exit == false) {
-    input = prompt("Enter an input (stats, level, feed, play, fly, exit) ")
+    if (obj.species == "bird") {
+        console.log(`Pleases select an option \n  Stats - Check up on ${obj.name} \n  Level - see ${obj.name}'s level and progress \n  Feed - give ${obj.name} food \n  Play - play with ${obj.name} \n  Fly - let ${obj.name} fly about \n  Exit - exit the application \n `)
+    }
+    else if (obj.species == "fish") {
+        console.log(`Pleases select an option \n  Stats - Check up on ${obj.name} \n  Level - see ${obj.name}'s level and progress \n  Feed - give ${obj.name} food \n  Play - play with ${obj.name} \n  Swim - let ${obj.name} Swim around \n  Exit - exit the application \n `)
+    }
+    input = prompt("Option: ")
+    console.log("\n")
     switch (input) {
-        case "stats":
+        case "Stats":
             obj.GetStats()
             break;
-        case "level":
+        case "Level":
             obj.Level()
             break;
-        case "feed":
+        case "Feed":
             obj.Feed()
             break;
-        case "play":
+        case "Play":
             obj.Play()
             break;
-        case "fly":
-            obj.Fly()
+        case "Fly","Swim":
+            if (obj.species == "bird") {
+                obj.Fly()
+            }
+            else if (obj.species == "fish") {
+                obj.Swim()
+            }
             break;
-        case "exit":
+        case "Exit":
             exit = true
             break
+        default:
+            console.log(`${input} is not a valid option, options are case-sensitive`)
     }
+
     if (obj.health > 100) {
         obj.health = 100;
     }
-    if (obj.hunger > 80) {
-        console.log(`${obj.name} is hungry`)
+    if (obj.hunger >= 100) {
+        console.log(` ${obj.name} is VERY hungry`)
+        obj.health = obj.health - 20
+        obj.hunger = 100
+    }
+    else if (obj.hunger > 80) {
+        console.log(` ${obj.name} is hungry`)
         obj.health = obj.health - 10
+    }
+    if (input != "Exit") {
+        prompt("press Enter to continue")
     }
 }
